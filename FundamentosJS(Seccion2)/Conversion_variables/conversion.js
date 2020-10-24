bc7cdbc666436260a4c345ffb77ac8f9ff8bca1e
Javier13g <61069624+Javier13g@users.noreply.github.com>
2020-10-24 13:42:24 -0400

    si

---
+++ FundamentosJS(Seccion2)/Conversion_variables/conversion.js
@@ -0,0 +1,12 @@
+//CONVERSION A STRING
+
+var Juego, FechaLanzamiento, Exito;
+
+Juego = "Pubg Lite";
+FechaLanzamiento = 2020;
+Exito = true;
+
+console.log("Nombre: " +Juego+ "\n"
+ +"Fecha de lanzamiento: " +FechaLanzamiento+ "\n"
+ + "Fue un exito?: " +Exito);
+
